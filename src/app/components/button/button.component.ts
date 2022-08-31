2902012c20a3985e7bd45e328da50e1637a20d65
slim <slimcodes@proton.me>
2022-08-31 23:41:15 +0100

    update `button` component

--- src/app/components/button/button.component.ts
+++ src/app/components/button/button.component.ts
@@ -3,17 +3,13 @@ import { Component, OnInit, Input } from '@angular/core';
 @Component({
   selector: 'app-button',
   templateUrl: './button.component.html',
-  styleUrls: ['./button.component.css']
+  styleUrls: ['./button.component.css'],
 })
 export class ButtonComponent implements OnInit {
+  @Input() text?: string;
+  @Input() color?: string;
 
-  @Input() text!: string;
-  @Input() color!: string;
+  constructor() {}
 
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
