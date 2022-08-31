637ccbae91bd19748cf00cf8460e1e67e0deb451
slim <slimcodes@proton.me>
2022-08-31 23:41:27 +0100

    update `header` component

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -3,19 +3,17 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css']
+  styleUrls: ['./header.component.css'],
 })
 export class HeaderComponent implements OnInit {
   title = 'Task-Tracker';
-  /* 
-    *REVIEW: should you define these properties in the class or in the `header` html
-  */
+  /*
+   *REVIEW: should you define these properties in the class or in the `header` html
+   */
   // text = 'Add';
   // color="green";
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
